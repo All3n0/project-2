47190cb046ecdba49bceb3632d86ebc843412f7d
W0lski <123263253+W0lski@users.noreply.github.com>
2024-05-07 08:44:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import routes from "./routes";
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
+const router = createBrowserRouter(routes);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <RouterProvider router={router}>
