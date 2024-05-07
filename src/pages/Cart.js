bf506c66eabe4bf844154e355bfc079b8e0c9904
ALLAN <allankipr6@gmail.com>
2024-05-07 12:51:33 +0300

    added cart and product crd to the home component and changed the add to cart procedure to aallow the desired output

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -2,10 +2,15 @@ import React from 'react';
 import CartItem from './CartItem';
 
 const Cart = ({ cartItems }) => {
+  const calculateTotalQuantity=(productId)=>{
+    return cartItems.reduce((total,item)=>{
+      return item.id ===productId? total+item.quantity:total;
+    })
+  }
   return (
     <div className="cart">
       {cartItems.map((item, index) => (
-        <CartItem key={index} productId={item.id} />
+        <CartItem key={index} productId={item.id} quantity={calculateTotalQuantity}/>
       ))}
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,21 @@
 import { useEffect, useState } from "react";
 import ProductCard from "../components/ProductCard";
 import NavBar from "../components/NavBar";
+import Cart from "./Cart";
 
 function Home() {
+  const [cartItems, setCartItems] = useState([]);
 
+  const addToCart = (product) => {
+    const existingItemIndex = cartItems.findIndex(item => item.id === product.id);
+    if (existingItemIndex !== -1) {
+      const updatedCartItems = [...cartItems];
+      updatedCartItems[existingItemIndex].quantity += 1;
+      setCartItems(updatedCartItems);
+    } else {
+      setCartItems(prevItems => [...prevItems, { id: product.id, quantity: 1 }]);
+    }
+  };
   return (
     <>
       <header>
@@ -11,7 +23,8 @@ function Home() {
         <h1>Home Page</h1>
       </header>
       <main>
-        {/* render components */}
+      <ProductCard addToCart={addToCart} />
+      <Cart cartItems={cartItems} />
       </main>
     </>
   );
